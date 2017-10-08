efe7b1efcaa1c3ae5b028f52ce879df75b682f0e
christianjurt <Longbeach@2>
2017-10-08 18:37:34 -0400

    initial Commit

---
+++ js/Main.js
@@ -0,0 +1,10 @@
+/*****************************************************************
+File: Main.js
+Author: Christian Jurt
+
+Description: This is a webiste for the Coder Dude Youtube Channel.
+
+Version: 0.0.1
+Updated: Oct 8, 2017
+
+*****************************************************************/
